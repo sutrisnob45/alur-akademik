369fad2f12d791ecb940cc9ccd4702d5142ba720
Bambang Sutrisno <sutrisnob45@gmail.com>
2019-06-15 13:01:10 +0700

    fix manajemen ruang

--- pages/master/tabel_ruang.php
+++ pages/master/tabel_ruang.php
@@ -1,9 +1,9 @@
-       <div class="box satu">
+       <div class="box">
         <div class="box-header">
           <h3 class="box-title">Tabel Ruang</h3>
         </div>
         <div class="box-header">
-          <button type="button" class="btn btn-primary toggle">Tambah</button>
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal" onclick="fillFormEdit('TAMBAH', this.id)">Tambah</button>
         </div>
         <!-- /.box-header -->
         <div class="box-body">
@@ -18,6 +18,23 @@
             </thead>
             <tbody>
               <?php
+              if (isset($_POST['tambah'])) {
+                $nama = $_POST['nama_ruang'];
+                $kode = $_POST['kode_posisi'];
+                mysqli_query($con,"INSERT into ruang VALUES(DEFAULT,'$nama','$kode')");
+              }
+              if (isset($_POST['edit'])) {
+                $id = $_POST['id_ruang'];
+                $nama = $_POST['nama_ruang'];
+                $kode = $_POST['kode_posisi'];
+                mysqli_query($con,"UPDATE ruang SET nama_ruang='$nama', posisi='$kode' WHERE id_ruang=$id");
+              }
+              if (isset($_POST['hapus'])) {
+                $id = $_POST['id_ruang'];
+                mysqli_query($con,"DELETE from ruang WHERE id_ruang='$id'");
+              }
+
+
               $rg=mysqli_query($con,"SELECT * FROM ruang");
               $i=1;
               while ($rgs= mysqli_fetch_array($rg)) :?>
@@ -27,10 +44,10 @@
                   <td><?= $rgs['nama_ruang'];?></td>
                   <td><?= $rgs['posisi'];?></td>
                   <td><center>
-                    <button type="button" class="btn btn-primary toggle toggle-edit" id="<?= 'baris' . $i;?>">edit</button>
-                    <button type="button" class="btn btn-danger" onclick="confirm('Hapus?')">hapus</button>
+                    <button type="button" class="btn btn-primary" onclick="fillFormEdit('EDIT', this.id)" id="<?= 'baris' . $i;?>" data-toggle="modal" data-target="#myModal">edit</button>
+                    <button type="button" class="btn btn-danger" data-toggle="modal" id="<?= 'baris' . $i;?>" data-target="#hapusModal" onclick="fillFormHapus(this.id)">hapus</button>
                     <!-- <a href="index.php?page=edit_ruang&id_ruang=<?php echo $rgs['id_ruang']?>" class="btn btn-primary">edit</a>  -->
-                    <!-- |<a href="index.php?page=hapus_ruang&id_ruang=<?php echo $rgs['id_ruang']?>" class="btn btn-primary" onclick="return confirm('yakin akan menghapus?');">hapus</a> --> 
+                    <!-- <a href="http://alur-akademik.com/master/hapus_ruang.php?id=<?= $rgs['id_ruang']?>" class="btn btn-danger" onclick="return confirm('yakin akan menghapus?');">hapus</a>  -->
                   </center></td>
                 </tr>
                 <?php
@@ -40,79 +57,72 @@
           </div>
           <!-- /.box-body -->
         </div>
-        <div class="box dua" style="display: none;">
-          <div class="box-header">
-            <h3 class="box-title">Kelola Ruang</h3>
-          </div>
-          <div class="box-header">
-           <div class="form-group">
-            <form action="" method="post">
-              <input type="hidden" id="id_ruang" name="id_ruang">
-              <div class="form-group">
-                <label for="nama_ruang">Nama</label>
-                <input type="text" class="form-control" id="nama_ruang" name="nama_ruang" placeholder="Nama Ruangan">
+        <!-- Modal konfimasi hapus -->
+        <div class="modal fade" id="hapusModal" >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-body">
+                Yakin untuk menghapus?
               </div>
-              <div class="form-group">
-                <label for="kode_posisi">Kode</label>
-                <input type="text" class="form-control" id="kode_posisi" name="kode_posisi" placeholder="Kode Posisi">
+              <div class="modal-footer">
+                <form action="" method="post">
+                  <input type="hidden" id="id_hapus" name="id_ruang">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                  <button type="submit" class="btn btn-danger" name="hapus">Hapus</button>
+                </form>
               </div>
             </div>
-            <button type="submit" class="btn btn-primary" name="tambah">Simpan</button>
-          </form>
-          <button type="button" class="btn btn-secondary toggle">Batal</button>
+          </div>
         </div>
-      </div>
-      <?php
-      if (isset($_POST['tambah'])) {
-        $nama = $_POST['nama_ruang'];
-        $kode = $_POST['kode_posisi'];
-        mysqli_query($con,"INSERT into ruang VALUES(DEFAULT,'$nama','$kode')");
-        header("Location: http://alur-akademik.com/master/tabel_ruang");
-      }
-      if (isset($_POST['edit'])) {
-        $id = $_POST['id_ruang'];
-        $nama = $_POST['nama_ruang'];
-        $kode = $_POST['kode_posisi'];
-        mysqli_query($con,"UPDATE ruang SET nama_ruang='$nama', posisi='$kode' WHERE id_ruang=$id");
-        header("Location: http://alur-akademik.com/master/tabel_ruang");
-      }
-      ?>
-      <script>
-        function displayEdit() {
-          $(".box.satu").toggle()
-          $(".box.dua").toggle()
-        }
-
-        function fillForm(mode = "DEFAULT", data_baris) {
-          if (mode === "edit") {
-            baris = $("#" + data_baris).parent().parent().siblings()
-            $("#id_ruang").val(baris[1].value)
-            $("#nama_ruang").val(baris[2].innerText)
-            $("#kode_posisi").val(baris[3].innerText)
-            $(":submit").attr("name","edit")
-          }else{
-            $("#id_ruang").val("")
-            $("#nama_ruang").val("")
-            $("#kode_posisi").val("")
-            $(":submit").attr("name","tambah")
-          }
-        }
-
-        $("button.toggle").each(function(){
-          $(this).on('click', function() {
-            if($(this).text() === "edit" ){
-              fillForm("edit", $(this)[0].id)
+        <!-- Modal kelola-->
+        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModal" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h3 class="modal-title" style="float: left;">Kelola Ruang</h3>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <div class="form-group">
+                  <form action="" method="post">
+                    <input type="hidden" id="id_ruang" name="id_ruang">
+                    <div class="form-group">
+                      <label for="nama_ruang">Nama</label>
+                      <input type="text" class="form-control" id="nama_ruang" name="nama_ruang" placeholder="Nama Ruangan">
+                    </div>
+                    <div class="form-group">
+                      <label for="kode_posisi">Kode</label>
+                      <input type="text" class="form-control" id="kode_posisi" name="kode_posisi" placeholder="Kode Posisi">
+                    </div>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                  <button type="submit" class="btn btn-primary">Simpan</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <script>
+          function fillFormEdit(mode = "DEFAULT", data_baris) {
+            if (mode === "EDIT") {
+              baris = $("#" + data_baris).parent().parent().siblings()
+              $("#id_ruang").val(baris[1].value)
+              $("#nama_ruang").val(baris[2].innerText)
+              $("#kode_posisi").val(baris[3].innerText)
+              $(":submit").attr("name","edit")
             }else{
-              fillForm($(this)[0].id)
+              $("#id_ruang").val("")
+              $("#nama_ruang").val("")
+              $("#kode_posisi").val("")
+              $(":submit").attr("name","tambah")
             }
-            displayEdit()
-          })
-        })
-
-        // $("button.toggle.toggle-edit").each(function(index, el) {
-        //   $(this).click(function() {
-        //     fillForm($(this)[0].id);
-        //   });
-        // });
-      </script>
-      
+          }
+          function fillFormHapus(data_baris){
+            baris = $("#" + data_baris).parent().parent().siblings()
+            $("#id_hapus").val(baris[1].value)
+          }
+        </script>
